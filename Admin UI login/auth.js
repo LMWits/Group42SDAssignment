6d3d0b4310ef13ad11762160553200b13ae51119
LMWits <2330429@students.wits.ac.za>
2025-04-23 21:56:15 +0200

    Updating href location

--- Admin UI login/auth.js
+++ Admin UI login/auth.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
       }
 
-      window.location.href = "userDashboard.html";
+      window.location.href = "../ZSFileAccesUpload1/public/adminHP.html";
 
     } catch (error) {
       console.error("❌ Microsoft Sign-In failed:", error.code, error.message);
