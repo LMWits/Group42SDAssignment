61f42c5534d0e896b8e90e91463992ac489b5f80
kbWits <2538031@students.wits.ac.za>
2025-04-26 14:39:29 +0200

    Add files via upload

--- Admin UI login/auth.js
+++ Admin UI login/auth.js
@@ -11,7 +11,6 @@ import {
   setDoc
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
-const db = getFirestore();
 
 function showMessage(message, pId){
     var errorMessage = document.getElementById(pId);
